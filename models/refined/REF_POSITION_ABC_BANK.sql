5d8a2300514abdbcce0f7ea4c08f445792138953
Juan Echavarria <juan.echavarria98@outlook.es>
2025-11-03 20:34:08 +0000

    update the ref_position_abc_bank model to read from a snapshot instead of STG

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,5 +1,12 @@
+WITH 
+current_from_snapshot as (
+    SELECT * FROM
+    {{ ref("SNSH_ABC_BANK_POSITION") }}
+    WHERE DBT_VALID_TO IS NULL
+)
+
 SELECT
     *
     ,POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT
     ,ROUND(UNREALIZED_PROFIT / COST_BASE, 5) as UNREALIZED_PROFIT_PCT
-FROM {{ ref('STG_ABC_BANK_POSITION')}}
+FROM current_from_snapshot
