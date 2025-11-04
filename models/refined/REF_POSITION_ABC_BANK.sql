b53fae9dd79787d8a3e2b14a930d677ea7e8e0ac
Juan Echavarria <juan.echavarria98@outlook.es>
2025-11-04 17:29:48 +0000

    creation of macro current_from_snapshot.

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,8 +1,8 @@
 WITH 
 current_from_snapshot as (
-    SELECT * FROM
-    {{ ref("SNSH_ABC_BANK_POSITION") }}
-    WHERE DBT_VALID_TO IS NULL
+    {{ current_from_snapshot(
+          snsh_ref = ref('SNSH_ABC_BANK_POSITION')
+       ) }}
 )
 
 SELECT
