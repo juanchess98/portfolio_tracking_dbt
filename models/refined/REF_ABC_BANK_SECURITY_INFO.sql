ebd7ce14bb35b9f295a7b1c30077f2d94332535d
Juan Echavarria <juan.echavarria98@outlook.es>
2025-11-04 17:42:00 +0000

    modified current_from_snapshot macro to add more flexiblity

--- models/refined/REF_ABC_BANK_SECURITY_INFO.sql
+++ models/refined/REF_ABC_BANK_SECURITY_INFO.sql
@@ -1,7 +1,8 @@
 WITH
 current_from_snapshot as (
     {{ current_from_snapshot(
-           snsh_ref=ref('SNSH_ABC_BANK_SECURITY_INFO')
+           snsh_ref=ref('SNSH_ABC_BANK_SECURITY_INFO'),
+           output_load_ts = false
      ) }}
 )
 SELECT *
