e9270f62e187274230c776e3500aaf166643fe65
Juan Echavarria <juan.echavarria98@outlook.es>
2025-11-04 18:00:29 +0000

    slight change to not include metadata column in the output

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,7 +1,8 @@
 WITH 
 current_from_snapshot as (
     {{ current_from_snapshot(
-          snsh_ref = ref('SNSH_ABC_BANK_POSITION')
+          snsh_ref = ref('SNSH_ABC_BANK_POSITION'),
+          output_load_ts = false
        ) }}
 )
 
